a222a83b812a722a00952c2d7ca8dc393392e2a9
Ziwei <xiaoxiaoweiiii@gmail.com>
2020-11-14 15:30:54 +0800

    Update Queue API - done
    fixed some errors in prev version

--- app.js
+++ app.js
@@ -56,16 +56,16 @@ const errors = {
         body: { error: 'Queue Id should be 10-character alphanumeric string', code: 'INVALID_JSON_BODY' },
         status: 400,
     },
-    //*i not sure how to do the err msg 
+    //#not sure how to show exactly the error, eg: error:unable to connect to database
     UNEXPECTED_SERVER_ERROR: {
-        body: { error: 'Unable to establish connection with database', code: 'UNEXPECTED_ERROR' },
+        body: { error: 'Unexpected server error', code: 'UNEXPECTED_ERROR' },
         status: 500,
     },
     OTHER_SERVER_ERROR: {
         body: { error: 'Unknown Internal Server Error', code: '' },
         status: 500,
     },
-    //Queue Id QUEUE12345 Not Found-not sure how to do the show id
+    //#Queue Id QUEUE12345 Not Found - not sure how to show the exact id
     UNKNOWN_QUEUE: {
         body: { error: 'Queue Id Not Found', code: 'UNKNOWN_QUEUE' },
         status: 404,
@@ -77,32 +77,27 @@ const errors = {
 };
 
 app.post('/company/queue', function (req, res) {
-    const body = JSON.stringify(req.body);
-    // console.log("object body %j", req.body);
-    const company_id = body.company_id;
-    const queue_id = body.queue_id;
+    const company_id = req.body.company_id;
+    const queue_id = req.body.queue_id;
+    console.log('company_id: ' + company_id + 'and queue_id:' + queue_id);
     //JSON validation
     var queueIdValidator = validator.isValid(queue_id, validator.checkQueueId);
     var check10digits = validator.isValid(company_id, validator.check10digit);
+    console.log('QueueId Validator: ' + queueIdValidator + ' and companyId Validator:' + check10digits);
     if (queueIdValidator && check10digits) {
-        //validation success
-        console.log('Queue Id validation success!');
+        console.log('Validation success!');
+        //connect to database
         database.createQueue(company_id, queue_id, function (err, result) {
-            //if database connection success, send the result
             if (!err) {
                 res.status(201).send(result);
-            } //if database connection failed, find out what is the error
+            }
             else {
-                client.query('SELECT * FROM Queue WHERE queue_id = ?', [queue_id]
-                    , function (err, rows) {
-                        if (err) {
-                            throw errors.UNEXPECTED_SERVER_ERROR;
-                        }
-                        //error: queue id already exists in database
-                        else if (rows.length) {
-                            throw errors.QUEUE_EXISTS;
-                        }
-                    });
+                if (result) {
+                    throw errors.QUEUE_EXISTS;
+                }
+                else {
+                    throw errors.UNEXPECTED_SERVER_ERROR;
+                }
             }
         });
     }//validation failed - queue_id
@@ -122,44 +117,47 @@ app.post('/company/queue', function (req, res) {
  */
 app.put('/company/queue/:queue_id', function (req, res) {
     var status = req.body.status;
-    const queue_id = req.params.queue_id; 
-    console.log("Status: "+status +" and queue: "+queue_id); // if got issue means what
+    const queue_id = req.params.queue_id;
+    console.log('Status: ' + status + 'and queue:' + queue_id);
     //JSON validation
     var queueIdValidator = validator.isValid(queue_id, validator.checkQueueId);
     var statusValidator = validator.isValid(status, validator.checkStatus);
-    // lets check our validator now wait the queueid nvr validate 10digits 
-    console.log("Queue Validator: "+queueIdValidator+" and Status Validator: "+statusValidator); // 
-    // both is false 
+    console.log('Queue Validator: ' + queueIdValidator + 'and Status Validator: ' + statusValidator);
     if (queueIdValidator && statusValidator) {
         console.log('Validation success!');
-        if(req.body.status == 'ACTIVATE'){
+        if (req.body.status == 'ACTIVATE') {
             status = '1';
-        }else{
+        } else {
             status = '0';
         }
+        //connect to database
         database.updateQueue(status, queue_id, function (err, result) {
             if (!err) {
-                //200 OK
-                res.status(200).send(result);
+                //unknown queue(queue_id not found in database)
+                if (result.length == 0) {
+                    console.log("UNKNOWN QUEUE")
+                    res.status(errors.UNKNOWN_QUEUE.status).send(errors.UNKNOWN_QUEUE.body);
+                } else {
+                    //success
+                    console.log("No error,result sent")
+                    res.status(200).send(result);
+                }
             } else {
-                //Except the else if for UNEXPECTED SERVER ERROR
-                //Non-existence Queue Id
-                if (!result) {
-                    throw errors.UNKNOWN_QUEUE;
-                }
-                else {
-                    throw errors.UNEXPECTED_SERVER_ERROR;
-                }
+                console.log("UNEXPECTED_SERVER_ERROR")
+                res.status(errors.UNEXPECTED_SERVER_ERROR.status).send(errors.UNEXPECTED_SERVER_ERROR.body);
             }
         });
-    } else if (!queueIdValidator) {
-        throw errors.INVALID_BODY_QUEUE;
     }
+    //queue_id wrong
+    else if (!queueIdValidator) {
+        res.status(errors.INVALID_BODY_QUEUE.status).send(errors.INVALID_BODY_QUEUE.body);
+    }
+    //status wrong
     else if (!statusValidator) {
-        throw errors.INVALID_BODY_STATUS;
+        res.status(errors.INVALID_BODY_STATUS.status).send(errors.INVALID_BODY_STATUS.body);
     }
     else {
-        throw errors.OTHER_SERVER_ERROR;
+        res.status(errors.OTHER_SERVER_ERROR.status).send(errors.OTHER_SERVER_ERROR.body);
     }
 })
 
